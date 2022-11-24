cdbd1e4b8ca4ffff7ac3289c273bb3296603dd46
phthartic <104261640+phthartic@users.noreply.github.com>
2022-11-24 20:58:45 +0800

    Add files via upload

---
+++ Birthday/main.js
@@ -0,0 +1,21 @@
+const main = document.querySelector('.main');
+const cancel = document.querySelector('.cancel');
+const msg = document.querySelector('#msg');
+
+while (true) {
+    let n = prompt("Enter your name");
+    
+    if(n == null) {
+        main.style.display = "none";
+        cancel.style.display = "flex";
+        msg.innerHTML = "Thank you!!!";
+    }
+
+    if (n.length > 0) {
+        alert("happy birthday catherine!");
+        alert('sana maging masaya ka today');
+        alert('salamat sa lahat ' + n + '!');
+        main.style.display = 'block';
+        break;
+    }
+}
